e810091d9ffdcbc5cf5299ebe1d4e33303d23f37
Tomasz Maj <tmaj.pl@gmail.com>
2020-01-27 20:33:16 +0100

    git init

---
+++ src/test/java/com/example/simpleclient/SimpleClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simpleclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
