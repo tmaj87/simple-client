b94c954d297d79a27ed0ef093d231dd51aea973e
Tomasz Maj <tmaj.pl@gmail.com>
2020-01-27 20:55:40 +0100

    spring boot admin, simple controller, actuator

--- src/main/java/com/example/simpleclient/Endpoints.java
+++ src/main/java/com/example/simpleclient/Endpoints.java
@@ -3,11 +3,19 @@ package com.example.simpleclient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 @RestController
 public class Endpoints {
 
     @GetMapping("/hello")
     public String hello() {
         return "Hello Vegas!";
+    }
+
+    @GetMapping("/")
+    public void defaultEndpoint(HttpServletResponse response) throws IOException {
+        response.sendRedirect("/actuator");
     }
 }
